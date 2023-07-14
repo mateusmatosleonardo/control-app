d1a0b8a3aa68c80b295b9077772829047c792e15
mateus <m4teusmatos@gmail.com>
2023-07-14 20:45:57 -0300

    feat: create global types

---
+++ src/@types/types.d.ts
@@ -0,0 +1,8 @@
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "@env" {
+  export const PORT: string;
+  export const IP: string;
+}
